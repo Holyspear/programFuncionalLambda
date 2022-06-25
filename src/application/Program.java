3ebd7a7f37b346bceec19d986368dc3586ed0b46
Holyspear <holyspear@gmail.com>
2022-06-25 17:46:12 -0300

    Implementação do CleanCode com função LAMBDA na ìntegra, sem instanciar classe nem métodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -22,11 +21,19 @@ public class Program {
 		// Declarar um Comparator utilizando sintaxe de FUNÇÃO anônima,
 		// agora não preciso mais da CLASSE, posso utilizar função LAMBDA para isso.
 		// A seta para uma função lambda se chama ArrowFunction.
-		Comparator<Product> comp = (product1, product2) -> {
-			return product1.getName().toLowerCase().compareTo(product2.getName().toLowerCase());
-		};
+		/*
+		 * Comparator<Product> comp = (product1, product2) -> { return
+		 * product1.getName().toLowerCase().compareTo(product2.getName().toLowerCase());
+		 * };
+		 */
 
-		list.sort(comp);
+		// Pra finalizar, fizemos o cleanCode para uma função LAMBDA na íntegra.
+		// Eliminamos a instanciação da classe Comparator
+		// Incluimos dois argumentos de função product1 e product2
+		// O Compilador vai comparar o tipo da List e jogar para os argumentos
+		// O ArrowFunction vai executar a função com os argumentos e efetuar o return
+		// automático
+		list.sort((product1, product2) -> product1.getName().toLowerCase().compareTo(product2.getName().toLowerCase()));
 
 		for (Product product : list) {
 			System.out.println(product);
