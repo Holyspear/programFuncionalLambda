2017bd6db389dd4c1dc413d73fe39bc940b2afef
Holyspear <holyspear@gmail.com>
2022-06-26 15:19:16 -0300

    Implementando Predicado do Produto direto com expressão Lambda

--- src/application/ProgramPredicate.java
+++ src/application/ProgramPredicate.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import model.entities.Product;
 
@@ -19,11 +18,8 @@ public class ProgramPredicate {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Instancia um Product Predicate para utilizar expressão Lambda
-		Predicate<Product> pred = p -> p.getPrice() >= 100;
-		
-		//Implementando de uma declaração de Predicado do Produto no escopo do programa
-		list.removeIf(pred);
+		//Implementando Predicado do Produto direto com expressão Lambda.
+		list.removeIf(p -> p.getPrice() >= 100);
 		
 		for (Product product : list) {
 			System.out.println(product);
