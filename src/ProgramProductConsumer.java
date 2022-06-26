7d1c607e4a0cb482f3b02271ce5e3fa7be78ce9c
Holyspear <holyspear@gmail.com>
2022-06-26 15:59:59 -0300

    Declarando Consumer para Product no Formato Lambda com uma variável externa.

--- src/ProgramProductConsumer.java
+++ src/ProgramProductConsumer.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import model.entities.Product;
 
@@ -13,8 +14,13 @@ public class ProgramProductConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		//Declarando Consumer para Product no Formato Lambda com uma variável externa.
+		Double percentage = 1.1;
+		
+		Consumer<Product> priceUpdate = product -> product.setPrice(product.getPrice()*percentage);
+		
 		//Método NÃO estático implementando Consumer na classe Product
-		list.forEach(Product::nonStaticPriceUpdate);
+		list.forEach(priceUpdate);
 		//Nova forma de imprimir uma coleção utilizando Lambda
 		//Isso porque o println é um método estático da classe System
 		list.forEach(System.out::println);
