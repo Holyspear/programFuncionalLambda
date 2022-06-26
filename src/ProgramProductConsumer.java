2213e18ee644342a3d0b535349117e5e659df7f9
Holyspear <holyspear@gmail.com>
2022-06-26 16:01:37 -0300

    Chamando direto no forEach Consumer para Product no Formato Lambda com uma variável externa

--- src/ProgramProductConsumer.java
+++ src/ProgramProductConsumer.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 import model.entities.Product;
 
@@ -14,13 +13,11 @@ public class ProgramProductConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Declarando Consumer para Product no Formato Lambda com uma variável externa.
+		//Chamando direto no forEach Consumer para Product no Formato Lambda com uma variável externa.
 		Double percentage = 1.1;
 		
-		Consumer<Product> priceUpdate = product -> product.setPrice(product.getPrice()*percentage);
-		
 		//Método NÃO estático implementando Consumer na classe Product
-		list.forEach(priceUpdate);
+		list.forEach(product -> product.setPrice(product.getPrice()*percentage));
 		//Nova forma de imprimir uma coleção utilizando Lambda
 		//Isso porque o println é um método estático da classe System
 		list.forEach(System.out::println);
